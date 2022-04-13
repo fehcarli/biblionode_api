b26e5d363bbe9162c8bdc88f00e0bcd8dba7b989
Felipe Carli <lipecarli@gmail.com>
2022-04-12 21:31:21 -0300

    Adicionado DATABASE_URL

--- src/config/config.js
+++ src/config/config.js
@@ -19,18 +19,11 @@ module.exports = {
       logging: false,
     },
     production: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      url: process.env.DB_URL,
-      host: process.env.DB_HOST,
       dialect: 'postgres',
-      logging: false,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
+      use_env_variable: 'DATABASE_URL',
+      define: {
+        timestamps: false,
+        underscored: true,
       },
     },
   };
